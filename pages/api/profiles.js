30633fee75a5a601e8dee5b88a136629e6684d83
Cleudeir <82124121+Cleudeir@users.noreply.github.com>
2022-03-06 16:26:34 -0300

    Update profiles.js

--- pages/api/profiles.js
+++ pages/api/profiles.js
@@ -18,7 +18,7 @@ export default async function Profiles(req, res) {
       } = get[i];
 
       result.push({
-        personaname.replace(/'/g, ' '),
+        personaname.replace(/'/g, ''),
         avatarfull,
         loccountrycode,
         account_id,
