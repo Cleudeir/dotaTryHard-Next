985ab5c5ef91b182ac375aa8d3d837f5c95dc68b
Cleudeir <82124121+Cleudeir@users.noreply.github.com>
2022-03-11 23:57:41 -0300

    read debug

--- pages/api/players/[id].js
+++ pages/api/players/[id].js
@@ -9,7 +9,7 @@ export default async function Players(req, res) {
   const pull = await GetMatchHistory(account_id);
   if (pull.data) {
     const list = await ListPlayers(pull.data);
-    const qnt = 25;
+    const qnt = 10;
     const random = Math.floor(Math.random() * (list.length - qnt));
     const slice = list.slice(random, random + qnt);
     const result = slice.filter((x) => x !== account_id);
